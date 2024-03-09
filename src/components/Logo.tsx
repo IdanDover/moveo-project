88ccf332b852a90d1bbeeef323797f85f4499682
Idan Dover <idan.dava@gmail.com>
2024-03-09 18:53:22 +0200

    changed the logo svg path

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,7 +7,7 @@ function Logo() {
       className="block h-[10vh] w-[15vw] sm:h-[10vh] sm:w-[10vw]"
     >
       <img
-        src="/public/code-it-up-logo.svg"
+        src="/src/assets/logo.svg"
         alt="logo"
         className="object-contain w-full h-full invert-0 dark:invert"
       />
